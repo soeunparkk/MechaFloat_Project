1967a8cf97b515a15402fcc96f114cfa9cd0e4c7
namsuhan0322 <rnatpakfl@gmail.com>
2025-04-23 19:41:49 +0900

    [Fix] 풍선 안들고 있는 로봇 애니메이션으로 교체

--- Assets/Scripts/Manager/PlayerAnimationManager.cs
+++ Assets/Scripts/Manager/PlayerAnimationManager.cs
@@ -6,11 +6,13 @@ public class PlayerAnimationManager : MonoBehaviour
 {
     public Animator animator;
     public PlayerStateMachine stateMachine;
+    public PlayerJump playerJump;
 
     // 애니메이션 파라미터 이름들을 상수로 정의
     private const string PARAM_IS_MOVING = "IsMoving";
     private const string PARAM_IS_RUNNING = "IsRunning";
     private const string PARAM_IS_JUMPING = "IsJumping";
+    private const string PARAM_IS_GROUND = "IsGround";
     private const string PARAM_IS_FALLING = "IsFalling";
     private const string PARAM_IS_PICKING = "IsPicking";
 
@@ -28,6 +30,7 @@ public class PlayerAnimationManager : MonoBehaviour
             switch (stateMachine.currentState)
             {
                 case IdleState:
+                    animator.SetBool(PARAM_IS_GROUND, true);
                     break;
                 case MovingState:
                     animator.SetBool(PARAM_IS_MOVING, true);
@@ -38,9 +41,14 @@ public class PlayerAnimationManager : MonoBehaviour
                     break;
                 case JumpingState:
                     animator.SetBool(PARAM_IS_JUMPING, true);
+                    animator.SetBool(PARAM_IS_GROUND, false);
                     break;
                 case FallingState:
                     animator.SetBool(PARAM_IS_FALLING, true);
+                    if (playerJump.GetVerticalVelocity() == 0)
+                    {
+                        animator.SetBool(PARAM_IS_GROUND, true);
+                    }
                     break;
                 case PickupState:
                     animator.SetBool(PARAM_IS_PICKING, true);
@@ -53,8 +61,9 @@ public class PlayerAnimationManager : MonoBehaviour
     private void ResetAllBoolParameters()
     {
         animator.SetBool(PARAM_IS_MOVING, false);
-        animator.SetBool(PARAM_IS_RUNNING, false);
+        animator.SetBool(PARAM_IS_RUNNING, false); 
         animator.SetBool(PARAM_IS_JUMPING, false);
+        animator.SetBool(PARAM_IS_GROUND, false);
         animator.SetBool(PARAM_IS_FALLING, false);
         animator.SetBool(PARAM_IS_PICKING, false);
     }
