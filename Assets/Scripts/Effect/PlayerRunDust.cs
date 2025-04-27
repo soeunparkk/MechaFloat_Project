dbd52312b0d4e6fec418fd41c1ea646c2d00044c
soeunparkk <soeunpark13@gmail.com>
2025-04-27 23:59:02 +0900

    [fix] 파티클 수정

--- Assets/Scripts/Effect/PlayerRunDust.cs
+++ Assets/Scripts/Effect/PlayerRunDust.cs
@@ -1,56 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerRunDust : MonoBehaviour
 {
-    public ParticleSystem dustEffectPrefab;
-    public Transform footPosition; // 발 밑에 붙일 위치
-    private ParticleSystem dustEffectInstance;
-
-    private Rigidbody rb;
-    private bool isGrounded = true;
+    public ParticleSystem runDustEffect;
+    private PlayerState playerState;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        if (dustEffectPrefab != null && footPosition != null)
-        {
-            dustEffectInstance = Instantiate(dustEffectPrefab, footPosition.position, Quaternion.identity, footPosition);
-            dustEffectInstance.Stop();
-        }
-        else
-        {
-            Debug.LogError("DustEffectPrefab 또는 FootPosition 연결이 안 됐습니다.");
-        }
+        playerState = GetComponent<PlayerState>();
     }
 
     void Update()
     {
-        if (dustEffectInstance == null) return;
-
-        if (isGrounded && rb.velocity.magnitude > 1.0f)
+        if (playerState.isRunning)
         {
-            if (!dustEffectInstance.isPlaying)
-                dustEffectInstance.Play();
+            if (!runDustEffect.isPlaying)
+                runDustEffect.Play();
         }
         else
         {
-            if (dustEffectInstance.isPlaying)
-                dustEffectInstance.Stop();
+            if (runDustEffect.isPlaying)
+                runDustEffect.Stop();
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-            isGrounded = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-            isGrounded = false;
     }
 }
