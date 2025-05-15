462defab816999f1de9e1b63ad6629c3488cf920
namsuhan0322 <rnatpakfl@gmail.com>
2025-05-15 16:03:25 +0900

    [Fix] 색깔 게이트도 인터페이스 추가

--- Assets/Scripts/Gimmick/ColorGate.cs
+++ Assets/Scripts/Gimmick/ColorGate.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorGate : MonoBehaviour
+public class ColorGate : MonoBehaviour, ICheckTrigger
 {
     public Color GateColor; // 문 고유 색
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEntered(Collider other)
     {
         if (other.CompareTag("Player"))
         {
