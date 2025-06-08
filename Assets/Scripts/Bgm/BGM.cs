07b25c47fa7a21986e5c0cf6abbc5a85c6321b3e
fti11 <mxonstruck@gmail.com>
2025-06-09 02:02:26 +0900

    {Feat] bgm, sfx 설정 코드 작성

---
+++ Assets/Scripts/Bgm/BGM.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable] 
+public class BGM : MonoBehaviour
+{
+    public string name;
+    public AudioClip clip;
+}
