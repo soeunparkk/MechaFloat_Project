aaf46cbeef0acd3b4aa0c60fe74e6ae31359b365
namsuhan0322 <rnatpakfl@gmail.com>
2025-06-11 23:35:11 +0900

    [Fix] DontDestroyOnLoad 필요 없는 부분 제거

--- Assets/Scripts/Manager/SaveManager.cs
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -21,7 +21,6 @@ public class SaveManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
